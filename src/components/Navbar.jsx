ac2f5e9d36000570708da4644e6b415f4d2ed97b
Rohan Raj <pvrohanraj@gmail.com>
2024-07-04 11:14:47 +0530

    Added Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,25 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="nav-bar">
-      <Link to="/" className="nav-link">
-        Home
-      </Link>
-      <Link to="/login" className="nav-link">
-        Login
-      </Link>
-      <Link to="/register" className="nav-link">
-        Register
-      </Link>
-      <Link to="/menu" className="nav-link">
-        Menu
-      </Link>
-      <Link to="/logout" className="nav-link">
-        Logout
-      </Link>
+    <nav className="nav-top">
+      <div>Logo</div>
+      <div className="nav-bar">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/login" className="nav-link">
+          Login
+        </Link>
+        <Link to="/register" className="nav-link">
+          Register
+        </Link>
+        <Link to="/menu" className="nav-link">
+          Menu
+        </Link>
+        <Link to="/logout" className="nav-link">
+          Logout
+        </Link>
+      </div>
     </nav>
   );
 };
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,7 +1,10 @@
 import { Navigate, useLocation } from "react-router";
 import { useAuth } from "../../setup/auth/Auth";
 import { useEffect, useState } from "react";
-
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { FaRegBookmark } from "react-icons/fa";
+import { FaBookmark } from "react-icons/fa";
+import { IoIosAddCircleOutline } from "react-icons/io";
 import axios from "axios";
 
 import "./Form.css";
@@ -12,6 +15,7 @@ const Menu = () => {
   const { token } = useAuth();
   const location = useLocation();
   const [data, setData] = useState(null);
+  const [isFormVisible, setIsFormVisible] = useState(true);
   if (!token) {
     return <Navigate to="/login" replace state={{ from: location }} />;
   }
@@ -56,7 +60,6 @@ const Menu = () => {
       .delete(`http://localhost:3000/travel-items/${id}`)
       .then((res) => {
         console.log(res);
-        console.log("complete");
         fetchData();
       })
       .catch((e) => {
@@ -64,155 +67,177 @@ const Menu = () => {
       });
   };
 
+  const handleShow = () => {
+    console.log(isFormVisible);
+    setIsFormVisible(!isFormVisible);
+  };
+
   return (
     <div>
       <Navbar />
-      <ul>
+      <div>
         {data &&
           data.map((item) => {
             return (
-              <li key={item._id}>
-                {item.destination}
+              <div className="list-container" key={item._id}>
+                <div>
+                  <div>
+                    <span>To</span>
+                    {item.destination}
+                  </div>
+                  <div>
+                    <span>From</span> {item.start}{" "}
+                  </div>
+                </div>
                 <span
                   onClick={() => {
                     handleDelete(item._id);
                   }}
                 >
-                  Delete
+                  <MdOutlineDeleteOutline />
                 </span>{" "}
                 <span>Edit</span>
-              </li>
+              </div>
             );
           })}
-      </ul>
-      <div>
-        <div className="container">
-          <form onSubmit={handleForm}>
-            <div>
-              <div>Start</div>
-              <div>Destination</div>
-            </div>
-            <div>
-              <div>
-                <input
-                  value={formData["start"]}
-                  onChange={(e) => {
-                    const res = e.target.value;
-                    console.log(res);
-                    setFormData({ ...formData, start: res });
-                  }}
-                />
-              </div>
-              <div>
-                <input
-                  value={formData["destination"]}
-                  onChange={(e) => {
-                    const res = e.target.value;
-                    console.log(res);
-                    setFormData({ ...formData, destination: res });
-                  }}
-                />
-              </div>
-            </div>
-            <div>
-              <div>Start Date</div>
-              <div>End Date</div>
-              <div>Mode Of Travel</div>
-            </div>
-            <div>
-              <div>
-                <input
-                  type="date"
-                  value={formData["startDate"]}
-                  onChange={(e) => {
-                    const res = e.target.value;
-                    console.log(res);
-                    setFormData({ ...formData, startDate: res });
-                  }}
-                />
-              </div>
-              <div>
-                <input
-                  type="date"
-                  value={formData["endDate"]}
-                  onChange={(e) => {
-                    const res = e.target.value;
-                    console.log(res);
-                    setFormData({ ...formData, endDate: res });
-                  }}
-                />
-              </div>
 
-              <div>
-                <select
-                  name="travel"
-                  id="travel"
-                  value={formData["modeOfTravel"]}
-                  onChange={(e) => {
-                    const res = e.target.value;
-                    console.log(res);
-                    setFormData({ ...formData, modeOfTravel: res });
-                  }}
-                >
-                  <option value="Cruise">Cruise</option>
-                  <option value="Plane">Plane</option>
-                  <option value="Rail">Rail</option>
-                  <option value="Road">Road</option>
-                </select>
-              </div>
-            </div>
-            <div>
-              <div>Notes</div>
-              <div>Activities</div>
-            </div>
-            <div>
-              <div>
-                <input
-                  type="text"
-                  value={formData["notes"]}
-                  onChange={(e) => {
-                    const res = e.target.value;
-                    console.log(res);
-                    setFormData({ ...formData, notes: res });
-                  }}
-                />
-              </div>
-              <div>
-                <input
-                  type="text"
-                  value={formData["activities"]}
-                  onChange={(e) => {
-                    const res = e.target.value;
-                    console.log(res);
-                    setFormData({ ...formData, activities: res });
-                  }}
-                />
-              </div>
-
-              <div>
-                <span
-                  onClick={() => {
-                    console.log(formData.bookmarked);
-
-                    setFormData({
-                      ...formData,
-                      bookmarked: !formData["bookmarked"],
-                    });
-                  }}
-                >
-                  Bookmark
-                </span>
-              </div>
-            </div>
-
-            <div>
-              <div>
-                {" "}
-                <button>Add Plan</button>
-              </div>
-            </div>
-          </form>
+        <div>
+          <div onClick={handleShow}>
+            <IoIosAddCircleOutline />
+          </div>
         </div>
+      </div>
+
+      <div>
+        {isFormVisible && (
+          <div className="container">
+            <form onSubmit={handleForm}>
+              <div>
+                <div>Start</div>
+                <div>Destination</div>
+              </div>
+              <div>
+                <div>
+                  <input
+                    value={formData["start"]}
+                    onChange={(e) => {
+                      const res = e.target.value;
+                      console.log(res);
+                      setFormData({ ...formData, start: res });
+                    }}
+                  />
+                </div>
+                <div>
+                  <input
+                    value={formData["destination"]}
+                    onChange={(e) => {
+                      const res = e.target.value;
+                      console.log(res);
+                      setFormData({ ...formData, destination: res });
+                    }}
+                  />
+                </div>
+              </div>
+              <div>
+                <div>Start Date</div>
+                <div>End Date</div>
+                <div>Mode Of Travel</div>
+              </div>
+              <div>
+                <div>
+                  <input
+                    type="date"
+                    value={formData["startDate"]}
+                    onChange={(e) => {
+                      const res = e.target.value;
+                      console.log(res);
+                      setFormData({ ...formData, startDate: res });
+                    }}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="date"
+                    value={formData["endDate"]}
+                    onChange={(e) => {
+                      const res = e.target.value;
+                      console.log(res);
+                      setFormData({ ...formData, endDate: res });
+                    }}
+                  />
+                </div>
+
+                <div>
+                  <select
+                    name="travel"
+                    id="travel"
+                    value={formData["modeOfTravel"]}
+                    onChange={(e) => {
+                      const res = e.target.value;
+                      console.log(res);
+                      setFormData({ ...formData, modeOfTravel: res });
+                    }}
+                  >
+                    <option value="Cruise">Cruise</option>
+                    <option value="Plane">Plane</option>
+                    <option value="Rail">Rail</option>
+                    <option value="Road">Road</option>
+                  </select>
+                </div>
+              </div>
+              <div>
+                <div>Notes</div>
+                <div>Activities</div>
+              </div>
+              <div>
+                <div>
+                  <input
+                    type="text"
+                    value={formData["notes"]}
+                    onChange={(e) => {
+                      const res = e.target.value;
+                      console.log(res);
+                      setFormData({ ...formData, notes: res });
+                    }}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    value={formData["activities"]}
+                    onChange={(e) => {
+                      const res = e.target.value;
+                      console.log(res);
+                      setFormData({ ...formData, activities: res });
+                    }}
+                  />
+                </div>
+
+                <div>
+                  <span
+                    onClick={() => {
+                      console.log(formData.bookmarked);
+
+                      setFormData({
+                        ...formData,
+                        bookmarked: !formData["bookmarked"],
+                      });
+                    }}
+                  >
+                    {formData.bookmarked ? <FaBookmark /> : <FaRegBookmark />}
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  {" "}
+                  <button>Add Plan</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
       </div>
 
       <Footer />
